3c14cb3f1099bfa0acf88f46a2fe5335d61705d9
Ken Jonathan Pais <81014114+kenjpais@users.noreply.github.com>
2021-04-30 17:51:22 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,22 +1,28 @@
+#Importing requirements 
+
 import pandas as pd
 import re
 pd.set_option('display.max_colwidth', 200)
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics.pairwise import cosine_similarity         #using cosine similiarity algorithm for matching questions
 
+#Setting up conversational Training Data
 
 df = pd.read_csv('C:/Users/Admin/Desktop/L&T/STEPIn/MiniProjectPython/data.csv')
 convo = df.iloc[:, 0]
 
+#Matching Question-Answer pairs into a list
 
 clist = []
+
 def qa_pairs(x):
-    cpairs = re.findall(": (.*?)(?:$|\n)", x)
+    cpairs = re.findall(": (.*?)(?:$|\n)", x)          #regular expression
     clist.extend(list(zip(cpairs, cpairs[1:])))
 convo.map(qa_pairs);
 convo_frame = pd.Series(dict(clist)).to_frame().reset_index()
 convo_frame.columns = ['q', 'a']
 
+#Matching closest question to user_input 
 
 vectorizer = TfidfVectorizer(ngram_range=(1,3))
 vec = vectorizer.fit_transform(convo_frame['q'])
