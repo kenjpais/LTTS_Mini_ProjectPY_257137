d51d907980a674f484a60834198d1acfdec4ea28
Ken Jonathan Pais <81014114+kenjpais@users.noreply.github.com>
2021-05-01 14:35:36 +0530

    Update train_chatbot.py

--- Implementation/train_chatbot.py
+++ Implementation/train_chatbot.py
@@ -1,3 +1,12 @@
+"""
+  @file test.py
+  @author Ken Jonathan Pais (SF ID:257137)
+  @brief
+  @version 1.0
+  @date 2021-05-01
+  @copyright Copyright (c) 2021
+"""
+
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
